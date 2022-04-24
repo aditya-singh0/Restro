d81aa5a0c0e4b20059a8f480182767c3afa06a5b
Aditya <adityasinghw84it@gmail.com>
2022-04-24 16:15:29 +0530

    First

---
+++ chunks/description.php
@@ -0,0 +1,23 @@
+<section class="fdes">
+		<div class="section white center">
+			<h2 class="header" style="padding:20px; padding-bottom: 30px;">Resturant Powered By Delhi Technological University</h2>
+		      <div class="row container center">
+		        <div class="col center l8 s12">
+		        	<p> Since our modest beginnings in 2005 with a little space in  Shahbad Daulatpur, Main Bawana Road, Delhi, Restro ‘s development has been enlivened with the energy to cook and serve solid, Indian-roused takeout food.
+
+In contrast to other Indian eateries, Restro was made with the explicit expectation to appear as something else.
+
+We realize numerous individuals love Indian sustenance, yet a large number of them loathe or are unconscious of the regularly unfortunate fixings that make run-of-the-mill Indian nourishment taste so great.
+
+Our menu highlights things that utilization the sound and fragrant flavors, however, forgets the stuffing ghee, spread, oil, and overwhelming cream.
+
+Restro has developed to incorporate four superb takeout areas in Toronto with additional to come sooner rather than later. Our group takes pride in the way that we can furnish our new and faithful clients with extraordinary tasting Indian-roused nourishment that is not normal for that at some other Indian eatery you visit.
+
+We perceive that a few people are as yet searching for the run-of-the-mill Indian nourishment, and that is fine with us. Our disclaimer is that on the off chance that you’re anticipating overwhelming, slick, undesirable Indian nourishment, Restro isn’t the place for you.</p>
+		        </div>
+		        <div class="col center l4 s12">
+		        	<img height="300" width="auto" style="object-fit: contain;" src="images/17964.jpg" alt="">
+		        </div>
+		        
+		      </div>
+	</section>
